1235ca0dd20252d43cfc70b90938dee0a3f0d79e
raphael percy <raphaelisango.ir@gmail.com>
2023-06-14 03:23:10 +0300

    commit2

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-import ClientServer from "./client_server_iot";
-import PubSub from "./pub_sub_iot";
-import REDIS_PUBSUB from "./redis-pub-sub-server";
+import ClientServer from "./client_server_iot/index.js";
+import PubSub from "./pub_sub_iot/index.js";
+import REDIS_PUBSUB from "./redis-pub-sub-server/index.js";
+
+import { createRequire } from "module";
+const Require = createRequire(import.meta.url);
+
+import WebSocket from "ws";
+
+const amqp = Require("amqplib/callback_api");
+//MQTT Sub TEST....................................................................
+(() => {
+  return;
+  var mqtt = Require("mqtt");
+  let mqttSub = PubSub.createPubSub("mqtt", mqtt, "1883", "localhost", "Sub");
+
+  mqttSub.On("connect", {
+    topic: "percymiler",
+  });
+
+  mqttSub.On("message", {
+    OnMsgCallback: (topic, message) => {
+      //redispush
+      console.log(JSON.parse(message));
+    },
+  });
+})();
+
+//AMQP SUB TEST....................................................................
+(async () => {
+  return;
+  let amqpPub = PubSub.createPubSub("amqp", amqp, "5672", "localhost");
+
+  await amqpPub.connect();
+  await amqpPub.createChannel();
+
+  await amqpPub.consume(
+    "percymiler",
+    (message) => {
+      console.log(`[x] Received '${message.content.toString()}'`);
+    },
+    { noAck: true }
+  );
+})();
+
+//WEBSOCKET CLIENT
+(() => {
+  const source = "percymiler";
+  const url = `ws://localhost:3030?connectionKey=${source}`;
+
+  const WS = ClientServer.createClientServer("websocket");
+  const client = WS("client", source, url);
+
+  const data = {
+    destination: "5555", //to subscribe to
+    source: source, //to publish to
+    data: { speed: 3, power: "56 watt" },
+    command: ["create", "update", "stop"],
+    extradata: {},
+  };
+
+  client.setModel(data);
+
+  client.connect(WebSocket);
+  // client.send(WebSocket, data);
+})();
--- test.js
+++ test.js
@@ -1,1 +1,73 @@
+import ClientServer from "./client_server_iot/index.js";
+import PubSub from "./pub_sub_iot/index.js";
+import REDIS_PUBSUB from "./redis-pub-sub-server/index.js";
 
+import { createRequire } from "module";
+const Require = createRequire(import.meta.url);
+const amqp = Require("amqplib/callback_api"); //AMQP
+
+import { createServer } from "http"; //WS
+import { WebSocketServer } from "ws"; //WS
+
+//MQTT PUB TEST....................................................................
+(() => {
+  return;
+  var mqtt = Require("mqtt");
+
+  let mqttpub = PubSub.createPubSub("mqtt", mqtt, "1883", "localhost", "Pub");
+  mqttpub.On("connect", {
+    destination: "raphael", //to subscribe to
+    source: "percymiler", // to publish to
+    data: { speed: 5555, power: "5555 watt" },
+    command: ["create", "update", ""],
+    extradata: {},
+  });
+
+  setInterval(() => {
+    mqttpub.publish({
+      destination: "raphael", //to subscribe to
+      source: "percymiler", // to publish to
+      data: { speed: 5555, power: "5555 watt" },
+      command: ["create", "update", ""],
+      extradata: {},
+    });
+  }, 3000);
+  //console.log(mqttpub);
+})();
+//AMQP PUB TEST....................................................................
+(async () => {
+  return;
+  let amqpPub = PubSub.createPubSub("amqp", amqp, "5672", "localhost");
+
+  await amqpPub.connect();
+  await amqpPub.createChannel();
+
+  //const queue = "hello";
+  const message = {
+    destination: "raphael", //to subscribe to
+    source: "percymiler", // to publish to
+    data: { speed: 5555, power: "5555 watt" },
+    command: ["create", "update", ""],
+    extradata: {},
+  };
+
+  await amqpPub.assertQueue(message.source, { durable: false });
+
+  await setInterval(() => {
+    amqpPub.sendToQueue(message.source, JSON.stringify(message));
+    console.log(`[x] Sent '${JSON.stringify(message)}'`);
+  }, 3000);
+})();
+//WEBSOCKET SERVER
+(() => {
+  //const wss = new WebSocketServer({ server });
+  const WS = ClientServer.createClientServer("websocket");
+  const Server = WS("server");
+  Server.serverSide(3030, createServer, WebSocketServer, () =>
+    console.log("listening on port 3030")
+  ).execute("wss", {
+    message: (data) => {
+      console.log("received: %s", `message rx from user ${data}`);
+    },
+  });
+})();
